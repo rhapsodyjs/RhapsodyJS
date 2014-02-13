7f948e32adf42acde6244f967e402d89041b2836
talyssonoc <talyssonoc@gmail.com>
2014-02-13 10:46:31 -0200

    Began to implement unified Mongoose.Model and Backbone.Model in the same file, model validation.

--- app/models/User.js
+++ app/models/User.js
@@ -1,10 +1,33 @@
 var User = {
-	name: {
-		type: String,
+	attributes: {
+		name: String,
+		age: {
+			type: Number,
+			validations: ['notNegative']
+		}
 	},
-	age: {
-		type: Number
+
+	sharedMethods: {
+		fullInfo: function() {
+			return this.get('name') + ' - ' + this.get('age');
+		},
+		notNegative: function(value) {
+			return value >= 0;
+		}
+	},
+
+	clientMethods: {
+
+	},
+
+	serverMethods: {
+		
+	},
+
+	options: {
+		allowREST: true,
+		middlewares: []
 	}
 };
 
-module.exports = Rhapsody.newModel('User', User);
+module.exports = User;
