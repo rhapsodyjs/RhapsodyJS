31f95955d1e69ba9b252a26321e81bf5107ca88f
talyssonoc <a11023@bcc.unifal-mg.edu.br>
2014-02-14 03:26:40 -0200

    Remove app test folder.

--- app/app.js
+++
@@ -1,3 +0,0 @@
-var rhapsody = require('rhapsodyjs')(__dirname);
-
-rhapsody.open(4242);
--- app/config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  active: true,
-	host: 'localhost',
-	port: 27017,
-	name: 'rhapsody'
-};
