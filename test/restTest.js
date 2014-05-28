fd321bd9b731c12a9a1e979127c59aff6871828d
talyssonoc <talyssonoc@gmail.com>
2014-05-28 01:07:23 -0300

    HATEOAS in RESTful API partially implemented (lacking query support and remove restricted attributes)

--- test/restTest.js
+++ test/restTest.js
@@ -21,6 +21,8 @@ describe('RESTful API tests', function() {
 
       models['Group'] = server.requireModel('Group');
       models['MyGroup'] = server.requireModel('MyGroup');
+      models['User'] = server.requireModel('User');
+      models['Class'] = server.requireModel('Class');
 
   });
 
@@ -55,7 +57,7 @@ describe('RESTful API tests', function() {
 
     newGroup.save(function(err) {
       supertest(app)
-      .get('/data/Group/' + newGroup._id)
+      .get('/data/Group/' + newGroup.id)
       .expect(200)
       .end(function(err, res) {
 
@@ -65,7 +67,7 @@ describe('RESTful API tests', function() {
         expect(addedGroup.name).to.be.equal(returnedGroup.name);
         expect(addedGroup.registry).to.be.equal(returnedGroup.registry);
         expect(addedGroup.acronym).to.be.equal(returnedGroup.acronym);
-        expect(addedGroup._id).to.be.equal(returnedGroup._id);
+        expect(addedGroup.id).to.be.equal(returnedGroup.id);
         expect(returnedGroup.restrictedAttr).to.not.exist;
 
         done();
@@ -96,7 +98,7 @@ describe('RESTful API tests', function() {
         expect(addedGroup.name).to.be.equal(returnedGroup.name);
         expect(addedGroup.registry).to.be.equal(returnedGroup.registry);
         expect(addedGroup.acronym).to.be.equal(returnedGroup.acronym);
-        expect(addedGroup._id).to.be.equal(returnedGroup._id);
+        expect(addedGroup.id).to.be.equal(returnedGroup.id);
         expect(returnedGroup.restrictedAttr).to.not.exist;
 
         done();
@@ -252,6 +254,103 @@ describe('RESTful API tests', function() {
           done();
         });
 
+      });
+    });
+  });
+
+  it('Should find data from hasMany/belongsTo relation coming from the API', function(done) {
+    var User = models['User'],
+        Group = models['Group'];
+
+
+    var newGroup = new Group({
+      name: 'User Group',
+      registry: 1,
+      acronym: 'UG1',
+      restrictedAttr: 'I am a restricted attribute'
+    });
+
+    newGroup.save(function(err) {
+      expect(err).to.not.exist;
+
+      var newUserData = {
+        name: 'User1',
+        age: 42,
+        password: 'that is a bad password, kids, do not use it',
+      };
+
+      newGroup.users.create(newUserData, function(err, newUser) {
+        expect(err).to.not.exist;
+
+        supertest(app)
+        .get('/data/Group/' + newGroup.id + '/users')
+        .expect(200)
+        .end(function(err, res) {
+
+          var user = res.body[0];
+
+          expect(newUser.name).to.equal(user.name);
+          expect(newUser.age).to.equal(user.age);
+          // expect(user.password).to.not.exist;
+          
+          done();
+
+        });
+      });
+    });
+  });
+
+  it('Should find data from hasAndBelongsToMany relation coming from the API', function(done) {
+    var Class = models['Class'];
+
+
+    var newClass = new Class({
+      name: 'New Class'
+    });
+
+    newClass.save(function(err) {
+      expect(err).to.not.exist;
+
+      var newStudentData = {
+        name: 'User2',
+        age: 9001,
+        password: 'that is a bad password, kids, do not use it',
+      };
+
+      newClass.students.create(newStudentData, function(err, newStudent) {
+        expect(err).to.not.exist;
+
+        supertest(app)
+        .get('/data/Class/' + newClass.id + '/students')
+        .expect(200)
+        .end(function(err, res) {
+
+          newClass.students(function(err, students) {
+            expect(err).to.not.exist;
+            expect(students).to.exist;
+
+            expect(res.body[0].name).to.equal(students[0].name);
+            expect(res.body[0].age).to.equal(students[0].age);
+            expect(res.body[0].password).to.equal(students[0].password);
+
+            supertest(app)
+            .get('/data/User/' + students[0].id + '/classes')
+            .expect(200)
+            .end(function(err, res) {
+
+              newStudent.classes(function(err, classes) {
+                expect(err).to.not.exist;
+                expect(classes).to.exist;
+
+                expect(res.body[0].name).to.equal(classes[0].name);
+
+                done();
+
+              });
+            });
+
+          });
+        });
       });
     });
   });
